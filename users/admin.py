fec53b24f8826a0eef0c88a470ce2b2a3dd26061
ShohjahonOktamov <hackmaster2108uz@gmail.com>
2024-08-25 15:20:52 +0500

    modified:   core/settings.py modified:   core/urls.py modified:   requirements.txt modified:   users/admin.py modified:   users/models.py

--- users/admin.py
+++ users/admin.py
@@ -7,8 +7,11 @@ from .models import CustomUser
 @admin.register(CustomUser)  # administrator panelida ro'yxatdan o'tdan o'tkazish
 class CustomUserAdmin(UserAdmin):
     fieldsets = UserAdmin.fieldsets + (
-        (None, {'fields': ('middle_name',)}),
+        ('Additional info', {
+            'fields': ('middle_name', 'avatar',)
+        }),
     )
+
     list_display = ('id', 'username', 'email', 'first_name', 'last_name', 'middle_name')
     list_display_links = ('id', 'username', 'email')
     search_fields = ('username', 'email', 'first_name', 'last_name', 'middle_name')
