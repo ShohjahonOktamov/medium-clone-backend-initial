7027a0846e99a6fff3715e6b3ffd4325bd66f929
ShohjahonOktamov <hackmaster2108uz@gmail.com>
2024-08-26 15:29:42 +0500

    m

--- articles/views.py
+++ articles/views.py
@@ -1,9 +1,10 @@
+import logging
 from typing import Type
 
 from drf_spectacular.utils import extend_schema, extend_schema_view
 from rest_framework import viewsets
 
-from users.models import CustomUser
+logger = logging.getLogger(__name__)
 from .models import Article
 from .serializers import (
     ArticleCreateSerializer,
@@ -34,3 +35,33 @@ class ArticlesView(viewsets.ModelViewSet):
         if self.action == 'create':
             return ArticleCreateSerializer
         return ArticleDetailSerializer
+
+    def list(self, request, *args, **kwargs):
+        response = super().list(request, *args, **kwargs)
+        logger.debug(f"List Response status code: {response.status_code}")
+        logger.debug(f"List Response content: {response.data}")
+        return response
+
+    def retrieve(self, request, *args, **kwargs):
+        response = super().retrieve(request, *args, **kwargs)
+        logger.debug(f"Retrieve Response status code: {response.status_code}")
+        logger.debug(f"Retrieve Response content: {response.data}")
+        return response
+
+    def create(self, request, *args, **kwargs):
+        response = super().create(request, *args, **kwargs)
+        logger.debug(f"Create Response status code: {response.status_code}")
+        logger.debug(f"Create Response content: {response.data}")
+        return response
+
+    def update(self, request, *args, **kwargs):
+        response = super().update(request, *args, **kwargs)
+        logger.debug(f"Update Response status code: {response.status_code}")
+        logger.debug(f"Update Response content: {response.data}")
+        return response
+
+    def destroy(self, request, *args, **kwargs):
+        response = super().destroy(request, *args, **kwargs)
+        logger.debug(f"Destroy Response status code: {response.status_code}")
+        logger.debug(f"Destroy Response content: {response.data}")
+        return response
