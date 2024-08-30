572142ef5d93bbf3c7dcb8ed96db162ee374d9e2
ShohjahonOktamov <hackmaster2108uz@gmail.com>
2024-08-30 23:46:24 +0500

    m

--- articles/models.py
+++ articles/models.py
@@ -145,6 +145,6 @@ class FAQ(Model):
         verbose_name_plural: str = 'FAQs'
         ordering: list[str] = ["-created_at"]
 
-    question: TextField = TextField()
-    answer: RichTextField = RichTextField()
+    question: TextField = TextField(null=True, blank=True)
+    answer: RichTextField = RichTextField(null=True, blank=True)
     created_at: DateTimeField = DateTimeField(auto_now_add=True)
