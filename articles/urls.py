affa6a7d322da243cee05bb7dbadeae8bed303f0
ShohjahonOktamov <hackmaster2108uz@gmail.com>
2024-08-30 22:50:05 +0500

    m

--- articles/urls.py
+++ articles/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
 from .views import ArticlesView, TopicFollowView, CreateCommentsView, ArticleDetailCommentsView, CommentsView, \
-    FavoriteArticleView, ClapView
+    FavoriteArticleView, ClapView, ReportArticleView
 
 router = DefaultRouter()
 router.register(prefix='', viewset=ArticlesView, basename='articles')
@@ -14,5 +14,6 @@ urlpatterns: list = [
     path('<int:pk>/comments/', CreateCommentsView.as_view(), name='create-comment'),
     path('<int:pk>/detail/comments/', ArticleDetailCommentsView.as_view(), name='detail-article-comments'),
     path('<int:pk>/favorite/', FavoriteArticleView.as_view(), name='favorite-article'),
-    path('<int:pk>/clap/', ClapView.as_view())
+    path('<int:pk>/clap/', ClapView.as_view(), name="article-clap"),
+    path('<int:pk>/report/', ReportArticleView.as_view(), name="article-report")
 ]
